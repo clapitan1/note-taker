7f3b43527d75c5651fd303663f043db1d7678254
clap <cilapitan1@gmail.com>
2023-04-27 23:20:41 -0700

    app get

--- server.js
+++ server.js
@@ -6,3 +6,20 @@ const { v4: uuidv4 } = require("uuid");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
+  });
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+    const notesData = fs.readFileSync(path.join(__dirname, "db", "db.json"));
+    res.json(JSON.parse(notesData));
+});
+
